5430fbd2448c70b8b02501743696a5cdc8a122a5
wanghao1891 <wanghao1891@126.com>
2017-07-27 15:56:00 +0800

    .

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-app.use(express.static('/Users/huangling/wanghao/video'));
+app.use(express.static('/Volumes/data/backup/video'));
 
 function index(req, res, next) {
   //var out = '<div><a href="teletubbies/teletubbies-christmas.mp4"><img src="teletubbies/teletubbies-christmas.jpg" /></a></div>';
@@ -13,8 +13,24 @@ function index(req, res, next) {
     'teletubbies/teletubbies-rocking-chair',
 
     'in-the-night-garden/in-the-night-garden-igglepiggle-goes-visiting',
+    'in-the-night-garden/in-the-night-garden-where-is-the-pinky-ponk-going',
 
     'pocoyo/session-01-01-04',
+    'pocoyo/session-02-05-08',
+    'pocoyo/pocoyo-session-3-09-12',
+
+    'masha-and-the-bear/masha-and-the-bear-how-they-met',
+    'masha-and-the-bear/masha-and-the-bear-victory-cry-episode-47',
+
+    'peppa-pig/peppa-pig-flying-a-kite',
+
+    'waybuloo/waybuloo-tricky-kicky',
+    'waybuloo/waybuloo-moving-things',
+    'waybuloo/waybuloo-bongleberrybubbles',
+
+    'thomas-and-friends/thomas-and-friends-the-adventure-begins-gordons-grand-adventure',
+
+    'curious-george/curious-george-01',
 
     'ryan-toys-review/01-giant-balloons-supprise-toys-and-ball-pit-challenge-in-huge-poll-disney-toys',
     'ryan-toys-review/02-surprise-toys-challenge-star-wars-darth-vader-vs-ryan-toysreview-easter-egg-hunt-water-ballon-fight',
@@ -25,7 +41,11 @@ function index(req, res, next) {
     'little-baby-bum/wheels-on-the-bus-plus-lots-more-nursery-rhymes-54-minutes',
 
     'hulyan-maya/thomas-the-tank-engine-and-friends-trackmaster-trains',
-    'hulyan-maya/we-love-trains'
+    'hulyan-maya/we-love-trains',
+
+    'teeter-taught-animation/calendar-crayons-teach-months-of-the-year',
+
+    'busy-beavers/busy-beavers-color-songs'
   ];
 
   var out = '<html>';
